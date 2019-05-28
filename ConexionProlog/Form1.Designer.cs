94afb3ad65a8b1f4631466cb54e9fc353ab46d00
3xp1o1t <mrlol404@protonmail.com>
2019-05-28 10:52:54 -0500

    Version inicial del proyecto
    Conexion con Prolog basica, ejecuta una consulta
    retornando una salida, especificamente R.
    
    tamano(R,grande).

---
+++ ConexionProlog/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ConexionProlog
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.txtConsulta = new System.Windows.Forms.TextBox();
+            this.txtSalida = new System.Windows.Forms.RichTextBox();
+            this.lblSalida = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEjecutar.Location = new System.Drawing.Point(281, 49);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(97, 33);
+            this.btnEjecutar.TabIndex = 0;
+            this.btnEjecutar.Text = "Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            this.btnEjecutar.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // txtConsulta
+            // 
+            this.txtConsulta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConsulta.Location = new System.Drawing.Point(14, 8);
+            this.txtConsulta.Name = "txtConsulta";
+            this.txtConsulta.Size = new System.Drawing.Size(368, 26);
+            this.txtConsulta.TabIndex = 1;
+            // 
+            // txtSalida
+            // 
+            this.txtSalida.Location = new System.Drawing.Point(12, 100);
+            this.txtSalida.Name = "txtSalida";
+            this.txtSalida.Size = new System.Drawing.Size(366, 96);
+            this.txtSalida.TabIndex = 2;
+            this.txtSalida.Text = "";
+            // 
+            // lblSalida
+            // 
+            this.lblSalida.AutoSize = true;
+            this.lblSalida.Location = new System.Drawing.Point(12, 84);
+            this.lblSalida.Name = "lblSalida";
+            this.lblSalida.Size = new System.Drawing.Size(36, 13);
+            this.lblSalida.TabIndex = 3;
+            this.lblSalida.Text = "Salida";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 208);
+            this.Controls.Add(this.lblSalida);
+            this.Controls.Add(this.txtSalida);
+            this.Controls.Add(this.txtConsulta);
+            this.Controls.Add(this.btnEjecutar);
+            this.Name = "frmMain";
+            this.Text = "Prolog C# | JM - v 0.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEjecutar;
+        private System.Windows.Forms.TextBox txtConsulta;
+        private System.Windows.Forms.RichTextBox txtSalida;
+        private System.Windows.Forms.Label lblSalida;
+    }
+}
+
---
+++ ConexionProlog/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SbsSW.SwiPlCs;
+
+
+namespace ConexionProlog
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Environment.SetEnvironmentVariable("SWI_HOME_DIR", @"C:\\Program Files (x86)\\swipl");
+            Environment.SetEnvironmentVariable("Path", @"C:\\Program Files (x86)\\swipl\\bin");
+            string[] p = { "-q", "-f", @"BaseConocimiento.pl" };
+
+            if (!PlEngine.IsInitialized)
+            {
+                try
+                {                    
+                    PlEngine.Initialize(p);
+                    _ = MessageBox.Show("Conexion exitosa");
+
+                    //PlQuery consultaCostoPorTipo = new PlQuery("objeto(O)");
+                    //foreach (PlQueryVariables v in consultaCostoPorTipo.SolutionVariables)
+                    //{
+                    //    Console.WriteLine("Objecto: " + v["O"].ToString());
+                    //}
+
+                    //PlEngine.PlCleanup();
+                }
+                catch
+                {
+                    _ = MessageBox.Show("Conexion fallida");
+                }
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            PlQuery consulta;
+            PlQuery cargar = new PlQuery("cargar('BaseConocimiento.bd')");
+            cargar.NextSolution();
+            string consultita = txtConsulta.Text;
+            //consulta = new PlQuery("tamano(O,grande).");
+            consulta = new PlQuery(consultita);
+            // usar R como salida por el momento.
+            txtSalida.Text.Remove(0, txtSalida.TextLength);
+            foreach (PlQueryVariables v in consulta.SolutionVariables)
+            {                
+                //Console.WriteLine("R: " + v["R"].ToString());
+                txtSalida.AppendText("\nR: " + v["R"].ToString());
+            }
+
+            PlEngine.PlCleanup();
+        }
+    }
+}
